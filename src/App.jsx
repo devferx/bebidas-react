a023a301c1126436f9851fd5b49db8a085f8efc0
devferx <ferkki@hotmail.com>
2020-11-06 13:51:08 -0400

    componentes iniciales creados

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <div className="container mt-5">
+        <div className="row">
+          <Formulario />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="bg-alert">
+      <h1>Busca Recetas de bebidas</h1>
+    </header>
+  );
+};
+
+export default Header;
